70a143fa8e53cd962e214f4993fcf3ad21fcc464
Gerald7608 <gerardoarenas@hotmail.com>
2024-11-05 11:41:23 -0300

    modificacion "navigation"

--- app/src/main/java/com/geraldsoft/mypruebas/ui/screens/Navigation.kt
+++ app/src/main/java/com/geraldsoft/mypruebas/ui/screens/Navigation.kt
@@ -14,6 +14,7 @@ import com.geraldsoft.mypruebas.ui.screens.home.HomeScreen
 @Composable
 fun Navigation() {
 
+
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = "home") {
